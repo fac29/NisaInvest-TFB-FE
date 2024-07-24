2539a9e50e7e3673af574cd030f234745501b33d
diddydeeable <diddydeeable@gmail.com>
2024-07-24 19:16:59 +0100

    button variants

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,33 +1,43 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
+import { fn } from '@storybook/test';
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 
-// export default {
-//   title: 'Components/Button',
-//   component: Button,
-// };
-const meta: Meta<typeof Button> = {
+const meta = {
+	title: 'Button',
 	component: Button,
-};
-export default meta;
-type Story = StoryObj<typeof Button>;
+	parameters: {
+		// Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+		layout: 'centered',
+	},
 
-// Define your stories
+	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+	tags: ['autodocs'],
+	// More on argTypes: https://storybook.js.org/docs/api/argtypes
+	argTypes: {
+		backgroundColor: { control: 'color' },
+	},
+	// Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+	args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
 	args: {
-		input: 'Button',
+		primary: true,
+		label: 'Button',
+		backgroundColor: '#F5F1ED',
+		fontFamily: 'Source Sans Regular',
 	},
 };
 
 export const Secondary: Story = {
 	args: {
-		...Primary.args,
-		input: '😄👍😍💯',
-	},
-};
-
-export const Tertiary: Story = {
-	args: {
-		...Primary.args,
-		input: '📚📕📈🤓',
+		primary: false,
+		label: 'Button',
+		backgroundColor: '#DC602E',
 	},
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,44 @@
-export function Button({input}: {input: string}) {
-    return (
-      <button>{input}</button>
-    );
-  }
+export interface ButtonProps {
+	/**
+	 * Is this the principal call to action on the page?
+	 */
+	primary?: boolean;
+	/**
+	 * What background color to use
+	 */
+	backgroundColor?: string;
+
+    fontFamily?:string;
+	/**
+	 * How large should the button be?
+	 */
+	size?: 'small' | 'medium' | 'large';
+	/**
+	 * Button contents
+	 */
+	label: string;
+	/**
+	 * Optional click handler
+	 */
+	onClick?: () => void;
+}
+
+export const Button = ({
+	primary = true,
+	size = 'medium',
+	backgroundColor,
+	fontFamily,
+	label,
+	...props
+}: ButtonProps) => {
+	return (
+		<button
+			type='button'
+			className={['storybook-button', `storybook-button--${size}`].join(' ')}
+			style={{ backgroundColor, fontFamily }}
+			{...props}
+		>
+			{label}
+		</button>
+	);
+};

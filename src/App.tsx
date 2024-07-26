2a1f6b430e972c56201d6c1b0155d0d58f6b8992
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-07-26 15:09:57 +0100

    add shadcn form components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Textarea } from '@/components/ui/textarea';
+
 function App() {
 	return (
 		<>
 			<p>$ We are Nisa Invest. We are here to sort out your finances! $</p>
 			<Input></Input>
+			<RadioGroup defaultValue='option-one'>
+				<div className='flex items-center space-x-2'>
+					<RadioGroupItem value='option-one' id='option-one' />
+					<Label htmlFor='option-one'>Option One</Label>
+				</div>
+				<div className='flex items-center space-x-2'>
+					<RadioGroupItem value='option-two' id='option-two' />
+					<Label htmlFor='option-two'>Option Two</Label>
+				</div>
+			</RadioGroup>
+			<Textarea></Textarea>
 		</>
 	);
 }

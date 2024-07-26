1050f5e6ff908e86ab020e959a382453f39c1fe1
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-07-26 12:32:23 +0100

    remove input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { Input } from '@/components/ui/input';
-
 function App() {
 	return (
 		<>
 			<p>$ We are Nisa Invest. We are here to sort out your finances! $</p>
-			<Input></Input>
 		</>
 	);
 }

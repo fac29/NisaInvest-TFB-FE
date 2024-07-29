bb492eb52375f9707c1f1d2c753a82eca37556e7
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-07-29 17:53:19 +0100

    rename component and move to Home Page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,11 @@
+import Carousel from '@/components/CarouselQuote/CarouselQuote';
+import { quotes } from '@/components/CarouselQuote/data';
+
 function Home() {
 	return (
 		<>
 			<p>$ We are Nisa Invest. We are here to sort out your finances! $</p>;
+			<Carousel quotes={quotes} />
 		</>
 	);
 }

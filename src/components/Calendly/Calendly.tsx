f2ce39d721ae2979f6588b106fd375c96b89f4bc
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-07-29 17:34:39 +0100

    cleanup and add comments

--- src/components/Calendly/Calendly.tsx
+++ src/components/Calendly/Calendly.tsx
@@ -1,12 +1,9 @@
-import React, { Component } from 'react';
 import { InlineWidget } from 'react-calendly';
+
+/* This takes advantage of react-calendly library.
+Refer to https://www.npmjs.com/package/react-calendly for optional component props to further customise the widget as necessary */
+
 export function Calendly() {
-	// componentDidMount() {
-	// 	// whatever stuff you need here
-	// }
-	// componentWillUnmount() {
-	// 	// whatever cleanup stuff you need here
-	// }
 	return (
 		<div>
 			<InlineWidget url='https://calendly.com/fih-nisainvest?text_color=202328&primary_color=ff4a1c' />

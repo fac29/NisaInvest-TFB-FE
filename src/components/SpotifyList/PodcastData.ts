fca668c1f5f8436c7462c2c8fbad214dbaf1cdc8
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-07-30 18:50:58 +0100

    add SpotifyList component and podcast data

---
+++ src/components/SpotifyList/PodcastData.ts
@@ -0,0 +1,162 @@
+export const podcastEpisodes = [
+	{
+		name: 'EP31:Finding Rizq - Contentment and Small Pleasures',
+		uri: 'spotify:episode:48uDebBFSKEXcbDXK8cvNZ',
+		date: '2024-07-21',
+	},
+	{
+		name: 'EP30:Present Bias and Future Focused Money Decisions',
+		uri: 'spotify:episode:0oJ2knzth9TYJf22VRZuek',
+		date: '2024-07-12',
+	},
+	{
+		name: 'EP29:Payday Routines',
+		uri: 'spotify:episode:1HnjQlBcLRghHnPBw09Z1b',
+		date: '2024-07-05',
+	},
+	{
+		name: 'EP28:Balancing Returns vs Costs',
+		uri: 'spotify:episode:0AQUJgx19MYmHZwOPW0dIA',
+		date: '2024-06-26',
+	},
+	{
+		name: 'EP27:Tawakkul and Personal Finance - Eid Special',
+		uri: 'spotify:episode:1rEvxlZWdYyD1KLvXWbCdr',
+		date: '2024-06-16',
+	},
+	{
+		name: 'EP26:How to Build Financial Resilience',
+		uri: 'spotify:episode:0dG5BjRyTVionNT7vQObKK',
+		date: '2024-06-08',
+	},
+	{
+		name: 'EP25:Comparing Shariah Compliant Investment Platforms',
+		uri: 'spotify:episode:2p9on8RcMFboItU2u3Hrkz',
+		date: '2024-06-01',
+	},
+	{
+		name: 'EP24:First-time Halal Investing',
+		uri: 'spotify:episode:56Utk9M3ekDiXOQzUCQb2C',
+		date: '2024-05-25',
+	},
+	{
+		name: 'EP23:Understanding NHS Pensions',
+		uri: 'spotify:episode:2pe1IgoDJ8JeSq2ZjNExer',
+		date: '2024-05-17',
+	},
+	{
+		name: 'EP22:How to Check Credit Scores',
+		uri: 'spotify:episode:2CJ08Yj9nYRupDg2vLxy49',
+		date: '2024-05-10',
+	},
+	{
+		name: 'EP21:Mindful Spending Habits',
+		uri: 'spotify:episode:79MHGsVmvlapOUVuHE8Zdo',
+		date: '2024-05-03',
+	},
+	{
+		name: 'EP20:Investing in Islamic Bonds',
+		uri: 'spotify:episode:6ZRtb2KkP25EdkkEl1B4wE',
+		date: '2024-04-26',
+	},
+	{
+		name: 'EP19:Gold as an Investment',
+		uri: 'spotify:episode:1KAHylsto49yzhyx0uq46a',
+		date: '2024-04-19',
+	},
+	{
+		name: 'EP18:Taxes and Tax Returns',
+		uri: 'spotify:episode:3hk43SOOdaEhiiPf3X3s6m',
+		date: '2024-04-12',
+	},
+	{
+		name: 'EP17:Ramadan Series - Barakah and Blessings',
+		uri: 'spotify:episode:06zbbioBKil0UD0WtNd18R',
+		date: '2024-04-05',
+	},
+	{
+		name: 'EP16:Ramadan Series - Sabr and Patience',
+		uri: 'spotify:episode:0RMxWTLIarIM5hRgryAdyC',
+		date: '2024-03-29',
+	},
+	{
+		name: 'EP15:Ramadan Series - Sadaqah and Charitable Giving',
+		uri: 'spotify:episode:0WgHMOLSkH2uip2K3DiBJa',
+		date: '2024-03-08',
+	},
+	{
+		name: 'EP14:Paying Off Student Loans in Your 20s',
+		uri: 'spotify:episode:0yUeWq1LT91POZYpJjjj7o',
+		date: '2024-03-01',
+	},
+	{
+		name: 'EP13:How to Behave as an Investor',
+		uri: 'spotify:episode:628wfiGxLObkOXMsbUxtVl',
+		date: '2024-02-23',
+	},
+	{
+		name: 'EP12:More Detail on Halal Index Funds',
+		uri: 'spotify:episode:5loyndMd2sxBS63t8azmJP',
+		date: '2024-02-16',
+	},
+	{
+		name: 'EP11:Step-by-Step Guide to Investing in a Halal Index Fund',
+		uri: 'spotify:episode:4jAQucMct4q4xRAoDfwQlV',
+		date: '2024-02-09',
+	},
+	{
+		name: 'EP10:Digital Assets - Cryptocurrencies and NFTs',
+		uri: 'spotify:episode:2eCzOwSbVCM9f1wDcB6tJR',
+		date: '2024-02-02',
+	},
+	{
+		name: 'EP09:Discussing the Main Asset Classes',
+		uri: 'spotify:episode:7lfV8KwXbWAP8VGejL60OL',
+		date: '2024-01-26',
+	},
+	{
+		name: 'EP08:Preparing to Buy a Home',
+		uri: 'spotify:episode:4e3LgoMNqQ8TmnFOAiCE3n',
+		date: '2024-01-19',
+	},
+	{
+		name: 'EP07:Islamic Finance and Credit Cards',
+		uri: 'spotify:episode:6uMvez3IxHiXvBZOhmigs9',
+		date: '2024-01-12',
+	},
+	{
+		name: 'EP06:Managing Finances and Family Expectations',
+		uri: 'spotify:episode:4MjFPrXIPeAUk1oZ7YzhFj',
+		date: '2024-01-12',
+	},
+	{
+		name: 'EP05:Halal Investing and Understanding Risk',
+		uri: 'spotify:episode:6rTpTcE1MISmCBH0EKTXsS',
+		date: '2024-01-12',
+	},
+	{
+		name: 'EP04:Halal Pensions and Retirement Planning',
+		uri: 'spotify:episode:1lXKt6rSFroRM5anpsoZgb',
+		date: '2024-01-12',
+	},
+	{
+		name: 'EP03:More on Halal Investing and Personal Finance Tips',
+		uri: 'spotify:episode:1wRTbk3kJy6tH3i6uLfTQ7',
+		date: '2024-01-12',
+	},
+	{
+		name: 'EP02:Personal Finances as a Muslim Entrepreneur',
+		uri: 'spotify:episode:39166dNikKUtHU73M4C9QX',
+		date: '2024-01-12',
+	},
+	{
+		name: 'EP01:Halal Investing and Student Loans',
+		uri: 'spotify:episode:1xtw2tNCftIqvBAVJC0ogq',
+		date: '2024-01-10',
+	},
+	{
+		name: 'Trailer:Nisa Invest Tea Talk',
+		uri: 'spotify:episode:4S5aGq8tkOaxEESa0OFkkw',
+		date: '2024-11-23',
+	},
+];

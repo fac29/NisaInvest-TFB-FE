9e1c58262028b39dfde2f3f8a3d8428b02e5eef3
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-07-31 12:14:51 +0100

    correct advisor name

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
 			<p>$ We are Nisa Invest. We are here to sort out your finances! $</p>;
 			<Carousel quotes={quotes} />
 			<ProfileCard
-				name='Fahan Ibrahim Fashi'
+				name='Fahan Ibrahim-Hashi'
 				position='Financial Advisor'
 				imageUrl='placeholder'
 				bioPage='fahan'

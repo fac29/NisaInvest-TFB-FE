6c1ff2cbfa6f8c2b4add76857821c3180b1b2a9e
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-07-31 17:41:37 +0100

    change fonts

--- src/components/TestimonialCard/TestimonialCard.tsx
+++ src/components/TestimonialCard/TestimonialCard.tsx
@@ -10,11 +10,11 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ name, text }) => {
 	return (
 		<div className='bg-offWhite border-solid border-2 border-lilac rounded-xl max-w-full p-4 h-full flex flex-col justify-between'>
 			<div>
-				<p className='text-left text-base italic font-playfair'>{text}</p>
+				<p className='text-left text-base italic font-source-sans'>{text}</p>
 			</div>
 			<div>
 				<Separator className='bg-lilac mt-4 mb-2'></Separator>
-				<p className='text-left tracking-wide text-base font-semibold font-source-sans'>
+				<p className='text-left tracking-wide text-base font-semibold font-playfair'>
 					{name}
 				</p>
 			</div>

89e552f49a18e41879f74e19896191e4f2628086
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-07-31 13:07:10 +0100

    add carousel from Shadcn

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,16 @@
 import Carousel from '@/components/CarouselQuote/CarouselQuote';
 import { quotes } from '@/components/CarouselQuote/data';
+import TestimonialCard from '@/components/TestimonialCard/TestimonialCard';
 
 function Home() {
 	return (
 		<>
 			<p>$ We are Nisa Invest. We are here to sort out your finances! $</p>;
 			<Carousel quotes={quotes} />
+			<TestimonialCard
+				name='Deeqa'
+				text='“I kept having people tell me that personal finance was a topic that made them feel uneasy, and after the talk, they left feeling self-assured.”'
+			></TestimonialCard>
 		</>
 	);
 }

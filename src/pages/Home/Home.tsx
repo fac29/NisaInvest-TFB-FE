ba9475f9a0c98fda0ae448c6c7ac8c1743fe875f
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-07-31 16:08:42 +0100

    prettify home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -13,10 +13,14 @@ import { testimonials } from '@/components/CarouselTestimonial/Testimonials';
 function Home() {
 	return (
 		<>
-			<p>$ We are Nisa Invest. We are here to sort out your finances! $</p>
-			<CarouselQuote quotes={quotes} />
-			<div className='flex justify-center'>
-				<CarouselTestimonial testimonials={testimonials}></CarouselTestimonial>
+			<div className='h-screen flex flex-col items-center justify-around'>
+				<p>$ We are Nisa Invest. We are here to sort out your finances! $</p>
+				<CarouselQuote quotes={quotes} />
+				<div className='flex justify-center'>
+					<CarouselTestimonial
+						testimonials={testimonials}
+					></CarouselTestimonial>
+				</div>
 			</div>
 		</>
 	);

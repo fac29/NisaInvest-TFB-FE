82be5e5af2074c6543b898a376926a9dc44073b4
diddydeeable <diddydeeable@gmail.com>
2024-07-31 19:38:07 +0100

    added fetch data

---
+++ src/utils/dataTypes.tsx
@@ -0,0 +1,8 @@
+export interface User {
+    id: number,
+    created_at: Date,
+    email: string,
+    name: string,
+    password: string,
+
+}
---
+++ src/utils/fetchData.tsx
@@ -0,0 +1,43 @@
+import {useState, useEffect} from 'react';
+import { User } from './dataTypes';
+
+//defines the state types for the custom useFetch hook.
+interface FetchDataState {
+isLoading: boolean;
+error: Error | null;
+data: User[]| null
+
+}
+
+const useFetch = (url: string): FetchDataState => {
+const [data, setData] = useState<User[]|null>(null);
+const [error, setError] = useState<Error | null>(null);
+const [isLoading, setIsLoading] = useState<boolean>(true);
+
+useEffect(()=> {
+    const fetchData = async () => {
+        setIsLoading(true);
+        try {
+            const res = await fetch(url);
+            if (!res.ok) throw new Error(res.statusText)
+
+            const jsonData = await res.json();
+            console.log(jsonData)
+        //    setData(jsonData?.data ? jsonData : [])
+        setData(jsonData ?? [])
+        } catch (error : any) {
+            setError(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+    fetchData();
+}, [url])
+
+
+return { isLoading, error, data };
+
+}
+export default useFetch
+
+useFetch('https://nisa-invest-tfb-be.vercel.app/users/all')

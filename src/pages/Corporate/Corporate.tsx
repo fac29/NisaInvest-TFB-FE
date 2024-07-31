9c428b12c5ab0b0aa40c94e39fda3d9112df8eb2
diddydeeable <diddydeeable@gmail.com>
2024-07-31 13:02:29 +0100

    added more padding

--- src/pages/Corporate/Corporate.tsx
+++ src/pages/Corporate/Corporate.tsx
@@ -5,7 +5,9 @@ function Corporate() {
 	return (
 		<div className='container mx-auto flex items-center justify-center max-w-6xl text-center'>
 			<div className='px-8 w-full pt-8 '>
-				<h1 className='text-3xl font-bold font-playfair pb-8 text-lilac'>For Employers</h1>
+				<h1 className='text-3xl font-bold font-playfair pb-8 text-lilac'>
+					For Employers
+				</h1>
 				<h3 className='font-source-sans text-xl pb-8 '>
 					Financial wellness is the dignity of knowing our hardwork throughout
 					life helps us achieve our goals insh'Allah
@@ -16,24 +18,40 @@ function Corporate() {
 					</Button>
 				</div>
 
-
-				<h2 className='text-3xl font-bold font-playfair pb-8 text-lilac'> How we support you</h2>
+				<h2 className='text-3xl font-bold font-playfair pb-8 text-lilac'>
+					{' '}
+					How we support you
+				</h2>
 				<h3 className='font-source-sans pb-8'>
 					Financial wellness begins at work, and we can support your employees.
 				</h3>
 				<ul className='grid grid-cols-2 gap-4 pb-8'>
-					<li className='font-source-sans text-xl bg-burntOrange'> 1:1 guidance</li>
-					<li className='font-source-sans text-xl  bg-burntOrange'> digital tools</li>
-					<li className='font-source-sans text-xl  bg-burntOrange'> tailored presentations</li>
-					<li className='font-source-sans text-xl bg-burntOrange'> content and media</li>
+					<li className='font-source-sans text-xl bg-burntOrange'>
+						{' '}
+						1:1 guidance
+					</li>
+					<li className='font-source-sans text-xl  bg-burntOrange'>
+						{' '}
+						digital tools
+					</li>
+					<li className='font-source-sans text-xl  bg-burntOrange'>
+						{' '}
+						tailored presentations
+					</li>
+					<li className='font-source-sans text-xl bg-burntOrange'>
+						{' '}
+						content and media
+					</li>
 				</ul>
 				{/* <Carousel {quotes = {...quotes}}/> */}
 				<div className='px-28'>
 					<RequestDemoForm />
 				</div>
 
-				<div>
-					<h3 className='text-3xl font-bold font-playfair pb-8 text-lilac'>Our feedback</h3>
+				<div className='pt-8'>
+					<h3 className='text-3xl font-bold font-playfair pb-8 text-lilac'>
+						Our feedback
+					</h3>
 					<h2 className='font-source-sans text-xl pb-8'>
 						Here are some quotes from our most recent events
 					</h2>

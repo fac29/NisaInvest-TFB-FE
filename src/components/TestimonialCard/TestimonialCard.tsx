0b8a1b27015bba5d87dbcf0b12cf0b386437c683
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-07-31 13:05:00 +0100

    create TestimonialCard component

---
+++ src/components/TestimonialCard/TestimonialCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Separator } from '../ui/separator';
+
+interface TestimonialCardProps {
+	name: string;
+	text: string;
+}
+
+const TestimonialCard: React.FC<TestimonialCardProps> = ({ name, text }) => {
+	return (
+		<div className='bg-offWhite border-solid border-2 border-lilac rounded-xl max-w-[300px] p-4'>
+			<div>
+				<p className='text-left text-base italic font-playfair'>{text}</p>
+			</div>
+			<Separator className='bg-lilac mt-4 mb-2'></Separator>
+			<div>
+				<p className='text-left tracking-wide text-base font-semibold font-source-sans'>
+					{name}
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default TestimonialCard;

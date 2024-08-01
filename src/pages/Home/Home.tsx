3225f55a4c6f3f8cf0ddc8b7138bbbfce2d1a520
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-08-01 17:42:02 +0100

    prettify home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,24 +3,62 @@ import { quotes } from '@/components/CarouselQuote/data';
 import { CarouselTestimonial } from '@/components/CarouselTestimonial/CarouselTestimonial';
 import { testimonials } from '@/components/CarouselTestimonial/Testimonials';
 import { ProfileCard } from '@/components/ProfileCard/ProfileCard';
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
 
 function Home() {
 	return (
 		<>
-			<div className='min-h-screen h-fit flex flex-col items-center justify-around'>
-				<p>$ We are Nisa Invest. We are here to sort out your finances! $</p>
-				<CarouselQuote quotes={quotes} />
-				<div className='flex justify-center'>
-					<CarouselTestimonial
-						testimonials={testimonials}
-					></CarouselTestimonial>
+			<div className='min-h-screen h-fit'>
+				{/* Section #1 */}
+				<div className='h-screen w-full bg-lilac flex flex-col items-center justify-around'>
+					<div className='max-w-[950px] flex flex-col justify-between gap-6'>
+						<div className='flex gap-4'>
+							<div>
+								<h1 className='font-playfair italic font-bold text-offWhite text-6xl mb-6'>
+									Wealth management to help you tie your camel
+								</h1>
+								<p className='font-source-sans text-offWhite text-2xl'>
+									Tailored personal finance by and for Muslim women
+								</p>
+							</div>
+							<div className='flex-shrink-0 w-[200px] h-[200px] bg-offWhite'>
+								<img
+									className='w-full h-full object-cover'
+									src='/api/placeholder/200/200'
+									alt='Picture of a cartoon camel'
+								/>
+							</div>
+						</div>
+						<div className='flex gap-4'>
+							<Button variant={'outline'}>
+								<Link to={'/about'}>Learn More</Link>
+							</Button>
+							<Button variant={'outline'}>
+								<Link to={'/booking'}>Book My Session</Link>
+							</Button>
+						</div>
+					</div>
 				</div>
-				<ProfileCard
-					name='Fahan Ibrahim-Hashi'
-					position='Financial Advisor'
-					imageUrl='/Fahan_Ibrahim-Hashi.jpeg'
-					bioPage='fahan'
-				></ProfileCard>
+				{/* Section #2 */}
+				<div className='h-screen w-full bg-offWhite flex flex-col items-center justify-around'>
+					{' '}
+					<CarouselQuote quotes={quotes} />
+					<div className='flex justify-center'>
+						<CarouselTestimonial
+							testimonials={testimonials}
+						></CarouselTestimonial>
+					</div>
+				</div>
+				{/* Section #3 */}
+				<div className='h-screen w-full bg-offWhite flex flex-col items-center justify-around'>
+					<ProfileCard
+						name='Fahan Ibrahim-Hashi'
+						position='Financial Advisor'
+						imageUrl='/Fahan_Ibrahim-Hashi.jpeg'
+						bioPage='fahan'
+					></ProfileCard>
+				</div>
 			</div>
 		</>
 	);

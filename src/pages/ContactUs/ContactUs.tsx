27569d5bf4d8492d70458b0ed6b11534f74017b2
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-08-01 12:30:55 +0100

    add content to ContactUs

--- src/pages/ContactUs/ContactUs.tsx
+++ src/pages/ContactUs/ContactUs.tsx
@@ -1,5 +1,13 @@
+import { RequestDemoForm } from '@/components/RequestDemoForm/RequestDemoForm';
+
 function ContactUs() {
-	return <></>;
+	return (
+		<div className='min-h-screen flex flex-col justify-center items-center w-full'>
+			<div className='w-1/3'>
+				<RequestDemoForm></RequestDemoForm>
+			</div>
+		</div>
+	);
 }
 
 export default ContactUs;

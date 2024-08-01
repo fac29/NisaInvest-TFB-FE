e17a5b3f2507e42202ee1a0dc57933681b3c3a1a
diddydeeable <diddydeeable@gmail.com>
2024-08-01 16:02:49 +0100

    sorry oskar

--- src/utils/fetchData.tsx
+++ src/utils/fetchData.tsx
@@ -1,42 +1,38 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { User, Quote, Goal } from './dataTypes';
 
 //defines the state types for the custom useFetch hook.
-interface FetchDataState {
-isLoading: boolean;
-error: Error | null;
-//I don't think this is the best way to do this, any suggestions? maybe expending classes? SOMETHING!
-data: User[]| User | Quote[] | Quote | Goal | Goal[] |null
-
+interface FetchDataState<T> {
+	isLoading: boolean;
+	error: Error | null;
+	data: T | T[] | null;
 }
 
-const useFetch = (url: string): FetchDataState => {
-const [data, setData] = useState<User[]|null>(null);
-const [error, setError] = useState<Error | null>(null);
-const [isLoading, setIsLoading] = useState<boolean>(true);
+function useFetch<T>(url: string): FetchDataState<T> {
+	const [data, setData] = useState<T[] | null>(null);
+	const [error, setError] = useState<Error | null>(null);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 
-useEffect(()=> {
-    const fetchData = async () => {
-        setIsLoading(true);
-        try {
-            const res = await fetch(url);
-            if (!res.ok) throw new Error(res.statusText)
+	useEffect(() => {
+		const fetchData = async () => {
+			setIsLoading(true);
+			try {
+				const res = await fetch(url);
+				if (!res.ok) throw new Error(res.statusText);
 
-            const jsonData = await res.json();
-            console.log(jsonData)
-        //    setData(jsonData?.data ? jsonData : [])
-        setData(jsonData ?? [])
-        } catch (error : any) {
-            setError(error)
-        } finally {
-            setIsLoading(false)
-        }
-    }
-    fetchData();
-}, [url])
+				const jsonData = await res.json();
+				console.log(jsonData);
+				//    setData(jsonData?.data ? jsonData : [])
+				setData(jsonData ?? []);
+			} catch (error: any) {
+				setError(error);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		fetchData();
+	}, [url]);
 
-
-return { isLoading, error, data };
-
+	return { isLoading, error, data };
 }
 export default useFetch;

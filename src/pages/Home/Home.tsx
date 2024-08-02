920a62b15d782e99dcc099608e5c5155c70530e4
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-08-02 13:29:58 +0100

    removing widgets from home and pseudocode that was breaking build

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -24,49 +24,8 @@ function Home() {
 					bioPage='fahan'
 				></ProfileCard>
 			</div>
-			<div className='grid grid-cols-2 lg:grid-cols-4 gap-1 items-start justify-items-center'>
-				<WidgetHeader category='savings' heading='Emergency Savings'>
-					<Widget
-						category='savings'
-						description="I have at least 50% of one month's salary saved"
-						isCoreTask={true}
-					/>
-				</WidgetHeader>
-				<WidgetHeader category='expenses' heading='Managing Expenses'>
-					<Widget
-						category='expenses'
-						description='I can login online to all my utility providers'
-						isCoreTask={false}
-					/>
-				</WidgetHeader>
-				<WidgetHeader category='investing' heading='Investing in the Future'>
-					<Widget
-						category='investing'
-						description='I have a list of any outstanding debts'
-						isCoreTask={false}
-					/>
-				</WidgetHeader>
-				<WidgetHeader category='charity' heading='Giving Back'>
-					<Widget
-						category='charity'
-						description='I purify any interest I receive in my bank accounts'
-						isCoreTask={true}
-					/>
-					<Widget
-						category='charity'
-						description='I give back in person'
-						isCoreTask={false}
-					/>
-				</WidgetHeader>
-			</div>
 		</>
 	);
 }
 
 export default Home;
-
-function setWidget(headerCategory: string, widgets: Widget[]) {
-	widgets.map((widget) =>
-		widget.category == headerCategory ? <Widget /> : ''
-	);
-}

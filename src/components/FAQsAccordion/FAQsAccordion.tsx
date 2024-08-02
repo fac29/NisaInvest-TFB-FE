901662e62d92afd77257c345b9d1c31990d58b42
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-08-02 13:22:15 +0100

    imported accordion component and created bones of FAQ page

---
+++ src/components/FAQsAccordion/FAQsAccordion.tsx
@@ -0,0 +1,21 @@
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from '@/components/ui/accordion';
+
+function FAQsAccordion() {
+	return (
+		<Accordion type='single' collapsible>
+			<AccordionItem value='item-1'>
+				<AccordionTrigger>Is it accessible?</AccordionTrigger>
+				<AccordionContent>
+					Yes. It adheres to the WAI-ARIA design pattern.
+				</AccordionContent>
+			</AccordionItem>
+		</Accordion>
+	);
+}
+
+export default FAQsAccordion;

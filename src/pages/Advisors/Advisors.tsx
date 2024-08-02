d3bdeb3e80f5122b0ad31cd2332f9e6d3c4f6578
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-08-02 15:34:50 +0100

    started populating advisors page and downloaded tabs component from shadcn

--- src/pages/Advisors/Advisors.tsx
+++ src/pages/Advisors/Advisors.tsx
@@ -1,17 +1,27 @@
 import { ProfileCard } from '@/components/ProfileCard/ProfileCard';
+import { Button } from '@/components/ui/button';
 
 function Advisors() {
 	return (
-		<>
-			<div className='min-h-screen h-fit flex flex-col items-center justify-around'>
-				<ProfileCard
-					name='Fahan Ibrahim-Hashi'
-					position='Financial Advisor'
-					imageUrl='./Fahan_Ibrahim-Hashi.jpeg'
-					bioPage='fahan'
-				></ProfileCard>
+		<div className='container pt-28 mx-auto min-h-screen h-fit flex items-center justify-center'>
+			<div className='w-full max-w-md space-y-8'>
+				<div className='text-center space-y-4'>
+					<h1 className='font-playfair text-2xl font-bold'>
+						Meet the Nisa Invest Planners
+					</h1>
+					<h2>One-to-one financial guidance to support you at every stage</h2>
+					<Button variant={'outline'}>Book your session</Button>
+				</div>
+				<div className='flex flex-col items-center justify-around'>
+					<ProfileCard
+						name='Fahan Ibrahim-Hashi'
+						position='Financial Advisor'
+						imageUrl='./Fahan_Ibrahim-Hashi.jpeg'
+						bioPage='fahan'
+					></ProfileCard>
+				</div>
 			</div>
-		</>
+		</div>
 	);
 }
 

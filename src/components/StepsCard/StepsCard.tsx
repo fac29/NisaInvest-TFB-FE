47ebc28664bacd64295b80de05f75a927f395bb5
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-08-02 19:07:43 +0100

    added content to StepsCard

--- src/components/StepsCard/StepsCard.tsx
+++ src/components/StepsCard/StepsCard.tsx
@@ -1,9 +1,14 @@
-import { Card, CardHeader } from '../ui/card';
+import { Card, CardContent, CardHeader } from '../ui/card';
 
 function StepsCard() {
 	return (
-		<Card>
-			<CardHeader></CardHeader>
+		<Card className='flex flex-col items-center justify-center'>
+			<CardHeader>
+				<h3>Step 1</h3>
+			</CardHeader>
+			<CardContent>
+				<p>Book your free guidance session</p>
+			</CardContent>
 		</Card>
 	);
 }

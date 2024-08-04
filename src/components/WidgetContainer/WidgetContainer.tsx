599a3fa46500451fd707a66d026ec8e90a202f56
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-08-04 20:24:06 +0100

    add widget containers

---
+++ src/components/WidgetContainer/WidgetContainer.tsx
@@ -0,0 +1,57 @@
+import {
+	FaCloudRain,
+	FaMoneyBill,
+	FaHourglassHalf,
+	FaSeedling,
+	FaStar,
+} from 'react-icons/fa6';
+import { Badge } from '@/components/ui/badge';
+import { IconType } from 'react-icons/lib';
+
+interface ContainerProps {
+	category: 'savings' | 'expenses' | 'investing' | 'charity';
+	children: React.ReactNode;
+}
+
+interface Category {
+	category: string;
+	element: IconType;
+}
+
+const categoriesList: Category[] = [
+	{
+		category: 'savings',
+		element: FaCloudRain,
+	},
+	{
+		category: 'expenses',
+		element: FaMoneyBill,
+	},
+	{
+		category: 'investing',
+		element: FaHourglassHalf,
+	},
+	{
+		category: 'charity',
+		element: FaSeedling,
+	},
+];
+
+function WidgetContainer({ category, children }: ContainerProps) {
+	return (
+		<div className='h-fill w-fit lg:max-w-96 rounded-2xl flex flex-col items-start bg-offWhite'>
+			<div className='max-w-64 lg:max-w-96 w-full mx-auto my-4 rounded-t-xl p-3 text-offWhite font-bold mt-0'>
+				<div className='flex justify-center gap-3'>
+					<Badge variant='secondary' className='text-md'>
+						{categoriesList.map((item) =>
+							item.category == category ? <item.element /> : ''
+						)}
+					</Badge>
+				</div>
+			</div>
+			<div className='px-1'>{children}</div>
+		</div>
+	);
+}
+
+export default WidgetContainer;
--- src/pages/Account/Dashboard/Dashboard.tsx
+++ src/pages/Account/Dashboard/Dashboard.tsx
@@ -1,11 +1,12 @@
 import WidgetHeader from '@/components/WidgetHeader/WidgetHeader';
 import Widget from '@/components/Widget/Widget';
+import WidgetContainer from '@/components/WidgetContainer/WidgetContainer';
 
 function Dashboard() {
 	return (
 		<div className='container min-h-screen mx-auto p-4 space-y-8'>
 			{/* Section 1 */}
-			<div className='p-4 rounded-lg'>
+			<div className='p-4 pt-28 rounded-lg'>
 				<div className='flex gap-4 items-center bg-lilac rounded-lg p-4 mb-4'>
 					<div>
 						<p className='text-xl font-bold'>1</p>
@@ -52,46 +53,92 @@ function Dashboard() {
 			<div className='p-4 rounded-lg'>
 				<div className='flex gap-4 items-center bg-lilac rounded-lg p-4 mb-4'>
 					<div>
-						<p className='text-xl font-bold'>1</p>
+						<p className='text-xl font-bold'>2</p>
 					</div>
 					<h2 className='text-xl font-bold'>
 						My current focus is to inshAllah say that:
 					</h2>
 				</div>
 				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-					{[...Array(4)].map((_, colIndex) => (
-						<div key={colIndex} className='bg-white p-4 rounded'>
-							<h3 className='text-lg font-semibold mb-2'>
-								Widget {colIndex + 1}
-							</h3>
-							<p className='text-gray-600'>
-								Content Section, Widget {colIndex + 1}
-							</p>
-						</div>
-					))}
+					<WidgetContainer category='savings'>
+						<Widget
+							category='savings'
+							description="I have at least 50% of one month's salary saved"
+							isCoreTask={true}
+						/>
+					</WidgetContainer>
+					<WidgetContainer category='expenses'>
+						<Widget
+							category='expenses'
+							description='I can login online to all my utility providers'
+							isCoreTask={false}
+						/>
+					</WidgetContainer>
+					<WidgetContainer category='investing'>
+						<Widget
+							category='investing'
+							description='I have a list of any outstanding debts'
+							isCoreTask={false}
+						/>
+					</WidgetContainer>
+					<WidgetContainer category='charity'>
+						<Widget
+							category='charity'
+							description='I purify any interest I receive in my bank accounts'
+							isCoreTask={true}
+						/>
+						<Widget
+							category='charity'
+							description='I give back in person'
+							isCoreTask={false}
+						/>
+					</WidgetContainer>
 				</div>
 			</div>
 			{/* Section 3 */}
 			<div className='p-4 rounded-lg'>
 				<div className='flex gap-4 items-center bg-lilac rounded-lg p-4 mb-4'>
 					<div>
-						<p className='text-xl font-bold'>1</p>
+						<p className='text-xl font-bold'>3</p>
 					</div>
 					<h2 className='text-xl font-bold'>
 						These are the items I can work through:
 					</h2>
 				</div>
 				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-					{[...Array(4)].map((_, colIndex) => (
-						<div key={colIndex} className='bg-white p-4 rounded'>
-							<h3 className='text-lg font-semibold mb-2'>
-								Widget {colIndex + 1}
-							</h3>
-							<p className='text-gray-600'>
-								Content Section, Widget {colIndex + 1}
-							</p>
-						</div>
-					))}
+					<WidgetContainer category='savings'>
+						<Widget
+							category='savings'
+							description="I have at least 50% of one month's salary saved"
+							isCoreTask={true}
+						/>
+					</WidgetContainer>
+					<WidgetContainer category='expenses'>
+						<Widget
+							category='expenses'
+							description='I can login online to all my utility providers'
+							isCoreTask={false}
+						/>
+					</WidgetContainer>
+					<WidgetContainer category='investing'>
+						<Widget
+							category='investing'
+							description='I have a list of any outstanding debts'
+							isCoreTask={false}
+						/>
+					</WidgetContainer>
+					<WidgetContainer category='charity'>
+						<Widget
+							category='charity'
+							description='I purify any interest I receive in my bank accounts'
+							isCoreTask={true}
+						/>
+						<Widget
+							category='charity'
+							description='I give back in person'
+							isCoreTask={false}
+						/>
+					</WidgetContainer>
 				</div>
 			</div>
 		</div>

ba4b20c43448216faf477661b4360fefe6fed7f1
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-08-05 11:33:51 +0100

    change margins

--- src/components/WidgetContainer/WidgetContainer.tsx
+++ src/components/WidgetContainer/WidgetContainer.tsx
@@ -39,8 +39,8 @@ const categoriesList: Category[] = [
 
 function WidgetContainer({ category, children }: ContainerProps) {
 	return (
-		<div className='h-fill w-fit lg:max-w-96 rounded-2xl flex flex-col items-start bg-offWhite'>
-			<div className='max-w-64 lg:max-w-96 w-full mx-auto my-4 rounded-t-xl p-3 text-offWhite font-bold mt-0'>
+		<div className='h-fill w-full lg:max-w-96 rounded-2xl flex flex-col items-start bg-offWhite'>
+			<div className='max-w-64 lg:max-w-96 w-full mx-auto rounded-t-xl pt-3 text-offWhite font-bold mt-0'>
 				<div className='flex justify-center gap-3'>
 					<Badge variant='secondary' className='text-md'>
 						{categoriesList.map((item) =>
--- src/components/WidgetHeader/WidgetHeader.tsx
+++ src/components/WidgetHeader/WidgetHeader.tsx
@@ -45,8 +45,8 @@ const categoriesList: Category[] = [
 
 function WidgetHeader({ category, heading, children }: HeaderProps) {
 	return (
-		<div className='h-fit w-fit lg:max-w-96 rounded-2xl grid items-start bg-offWhite'>
-			<div className='max-w-64 lg:max-w-96 w-full mx-auto my-4 rounded-t-xl p-3 shadow-lg bg-lilac text-offWhite font-bold mt-0'>
+		<div className='h-fit w-full lg:max-w-96 rounded-2xl grid items-start bg-offWhite'>
+			<div className='max-w-64 lg:max-w-96 w-full mx-auto rounded-t-xl p-3 shadow-lg bg-lilac text-offWhite font-bold mt-0'>
 				<div className='flex justify-center gap-3'>
 					<Badge variant='secondary' className='text-md'>
 						{categoriesList.map((item) =>

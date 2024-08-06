412f19299a54d8d0021946362e91d1588f5183a8
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-08-06 13:05:59 +0100

    cleanup

--- src/pages/Account/Dashboard/Dashboard.tsx
+++ src/pages/Account/Dashboard/Dashboard.tsx
@@ -1,80 +1,8 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import WidgetHeader from '@/components/WidgetHeader/WidgetHeader';
-import Widget from '@/components/Widget/Widget';
-import WidgetContainer from '@/components/WidgetContainer/WidgetContainer';
-import useFetch from '@/utils/fetchData';
-import {
-	DashboardContainer,
-	DashboardSection,
-} from '@/components/Dashboard/Dashboard';
 import { DashboardLayout } from '@/components/Dashboard/Dashboard';
-
-const baseUrl = import.meta.env.VITE_BASE_URL;
-
-// Define the types for the goals data structure
-interface Goal {
-	id: number;
-	title: string;
-	category: 'savings' | 'expenses' | 'investing' | 'charity';
-	created_at: string;
-	sort_order: number;
-	description: string;
-	is_recurrent: boolean;
-	recurrence_type: string | null;
-	recurrence_value: string | null;
-	status: 'completed' | 'focused' | 'not_done' | null;
-}
-
-interface CategorizedGoal {
-	category: 'savings' | 'expenses' | 'investing' | 'charity';
-	goals: Goal[];
-}
-
-interface GoalsData {
-	message: string;
-	categorizedGoals: CategorizedGoal[];
-}
 
 function Dashboard() {
 	const userId = 1; // this should be checked against the logged in user
-
-	const goalsData = useFetch<GoalsData>(`${baseUrl}/goals/user/${userId}`)
-		.data as GoalsData;
-
-	/* 	function to filter through user goals based on status and category in order to populate the dashboard sections. 
-	Usage eg:
-	{addWidgets(goalsData, 'completed', 'savings')} 
-	*/
-
-	function addWidgets(
-		data: GoalsData,
-		status?:
-			| ('completed' | 'focused' | 'not_done' | null)[]
-			| ('completed' | 'focused' | 'not_done' | null),
-		category?: 'savings' | 'expenses' | 'investing' | 'charity'
-	) {
-		if (!data || !data.categorizedGoals) return null;
-		console.log(data);
-		const matchingWidgets = data.categorizedGoals
-			.filter((categorizedGoal) => categorizedGoal.category === category)
-			.flatMap((categorizedGoal) => categorizedGoal.goals)
-			.filter((goal) => {
-				if (Array.isArray(status)) {
-					return status.includes(goal.status);
-				}
-				return goal.status === status;
-			})
-			.map((goal) => (
-				<Widget
-					key={goal.id}
-					category={goal.category}
-					description={goal.description}
-					isCoreTask={goal.is_recurrent}
-				/>
-			));
-
-		return matchingWidgets;
-	}
 
 	return (
 		<Tabs defaultValue='dashboard' className='flex flex-col items-center'>

a24ef45891a96df474a8a6510d50ef30a11173eb
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-08-06 16:24:42 +0100

    amended styling

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -78,10 +78,10 @@ const NavigationMenuContent = React.forwardRef<
 	<NavigationMenuPrimitive.Content
 		ref={ref}
 		className={cn(
-			'right-0 absolute',
-			'rounded-md border shadow-lg',
-			'absolute top-full min-w-36 w-full overflow-hidden bg-popover mt-[5px]',
+			'left-0 absolute',
+			'absolute top-full w-fit bg-popover mt-[5px]',
 			'data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52',
+			'origin-top-center absolute mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)]',
 			className
 		)}
 		{...props}

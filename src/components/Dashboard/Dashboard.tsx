11e122487c46da13f10c1968a6367eedec6fc24c
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-08-08 00:00:33 +0100

    make dashboard collapsible

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,15 @@
-import { Loader2 } from 'lucide-react';
+import { Loader2, ChevronsUpDown } from 'lucide-react';
 import React, { ReactElement, useEffect, useState, useCallback } from 'react';
 import WidgetHeader from '@/components/WidgetHeader/WidgetHeader';
 import WidgetContainer from '@/components/WidgetContainer/WidgetContainer';
 import useFetch from '@/utils/fetchData';
 import Widget from '@/components/Widget/Widget';
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from '@/components/ui/collapsible';
+import { Button } from '../ui/button';
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
@@ -97,21 +103,44 @@ export function DashboardContainer({ children }: DashboardContainerProps) {
 	const sections = React.Children.toArray(children);
 
 	return (
-		<div className='container min-h-screen mx-auto p-4 space-y-8'>
+		<div className='container min-h-screen mx-auto p-4'>
 			{sections.map((section, index) => {
 				if (React.isValidElement<DashboardSectionProps>(section)) {
+					const [isOpen, setIsOpen] = React.useState(true);
 					return (
-						<div key={index} className='p-4 rounded-lg'>
-							<div className='flex gap-4 items-center bg-lilac rounded-lg p-4 mb-4'>
-								<div className='flex items-center justify-center w-10 h-10 bg-offWhite rounded-full'>
-									<p className='text-xl font-bold'>{index + 1}</p>
+						<Collapsible
+							open={isOpen}
+							onOpenChange={setIsOpen}
+							key={index}
+							className='lg:min-w-[1000px]'
+						>
+							<div key={index} className='p-4 rounded-lg'>
+								<div className='flex gap-4 items-center bg-lilac rounded-lg p-2 mb-4'>
+									<div className='flex items-center justify-center w-10 h-10 bg-offWhite rounded-full'>
+										<p className='text-xl font-bold'>{index + 1}</p>
+									</div>
+									<div className='w-full flex justify-between items-center'>
+										<h2 className='w-fit text-xl font-bold'>
+											{section.props.title}
+										</h2>
+										<CollapsibleTrigger>
+											<Button
+												variant='ghost'
+												size='sm'
+												className='w-9 p-0 hover:bg-offWhite hover:bg-opacity-20'
+											>
+												<ChevronsUpDown className='h-4 w-4' />
+											</Button>
+										</CollapsibleTrigger>
+									</div>
 								</div>
-								<h2 className='text-xl font-bold'>{section.props.title}</h2>
+								<CollapsibleContent>
+									<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+										{section.props.children}
+									</div>
+								</CollapsibleContent>
 							</div>
-							<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-								{section.props.children}
-							</div>
-						</div>
+						</Collapsible>
 					);
 				}
 				return null;

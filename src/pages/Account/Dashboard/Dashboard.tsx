f574e794a9d2495b5987254bbe41690968bc2686
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-08-07 18:42:03 +0100

    added faqs to dashboard

--- src/pages/Account/Dashboard/Dashboard.tsx
+++ src/pages/Account/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { DashboardLayout } from '@/components/Dashboard/Dashboard';
 import { MyProfile } from '@/components/MyProfile/MyProfile';
+import FAQsAccordion from '@/components/FAQsAccordion/FAQsAccordion';
 
 function Dashboard() {
 	const userId = 1; // this should be checked against the logged in user
@@ -11,6 +12,7 @@ function Dashboard() {
 				<TabsTrigger value='dashboard'>Dashboard</TabsTrigger>
 				<TabsTrigger value='my_profile'>My Profile</TabsTrigger>
 				<TabsTrigger value='notes'>Notes</TabsTrigger>
+				<TabsTrigger value='faqs'>FAQs</TabsTrigger>
 			</TabsList>
 			<TabsContent value='dashboard'>
 				<DashboardLayout userId={userId} />
@@ -23,6 +25,14 @@ function Dashboard() {
 			<TabsContent value='notes'>
 				<div className='min-h-screen'>
 					<p>Notes go Here</p>
+				</div>
+			</TabsContent>
+			<TabsContent value='faqs'>
+				<div className='min-h-screen flex flex-col items-center justify-center space-y-8 max-w-md'>
+					<h2 className='font-playfair font-bold text-2xl'>
+						Frequently Asked Questions
+					</h2>
+					<FAQsAccordion />
 				</div>
 			</TabsContent>
 		</Tabs>

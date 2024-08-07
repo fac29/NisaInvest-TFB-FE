1d8b50ef549d3c9f50d11a9d780fc660652d4443
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-08-07 12:11:41 +0100

    fixed homepage formatting

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,69 +3,65 @@ import { quotes } from '@/components/CarouselQuote/data';
 import { CarouselTestimonial } from '@/components/CarouselTestimonial/CarouselTestimonial';
 import { testimonials } from '@/components/CarouselTestimonial/Testimonials';
 import { ProfileCard } from '@/components/ProfileCard/ProfileCard';
-import image from '/fahan_profile.png'
+import fahanImage from '/fahan_square.jpg';
 import { Button } from '@/components/ui/button';
 import { Link } from 'react-router-dom';
 
 function Home() {
-	return (
-		<main className='min-h-screen h-fit'>
-			{/* Section #1 */}
-			<div className='h-screen w-full bg-lilac flex flex-col items-center justify-around'>
-				<div className='max-w-[950px] flex flex-col justify-between gap-6'>
-					<div className='flex gap-4'>
-						<div>
-							<h1 className='font-playfair italic font-bold text-offWhite text-6xl mb-6'>
-								Wealth management to help you tie your camel
-							</h1>
-							<p className='font-source-sans text-offWhite text-2xl'>
-								Tailored personal finance by and for Muslim women
-							</p>
-						</div>
-						<div className='flex-shrink-0 w-[200px] h-[200p]'>
-							<img
-								className='w-full h-full object-cover'
-								src='./camel_logo.png'
-								alt='Picture of a cartoon camel'
-							/>
-						</div>
-					</div>
-					<div className='flex gap-4'>
-						<Button variant={'outline'}>
-							<Link to={'/about'}>Learn More</Link>
-						</Button>
-						<Button variant={'outline'}>
-							<Link to={'/booking'}>Book My Session</Link>
-						</Button>
-					</div>
-				</div>
-			</div>
-			{/* Section #2 */}
-			<div className='h-screen w-full bg-offWhite flex flex-col items-center justify-around'>
-				<CarouselQuote quotes={quotes} />
-			</div>
-			{/* Section #3 */}
-			<div className='h-screen w-full bg-offWhite flex flex-col items-center justify-around'>
-				<ProfileCard
-					name='Fahan Ibrahim-Hashi'
-					position='Financial Advisor'
-					imageUrl='/Fahan_Ibrahim-Hashi_square.jpg'
-					bioPage='fahan'
-				></ProfileCard>
-				<div className='flex justify-center'>
-					<CarouselTestimonial
-						testimonials={testimonials}
-					></CarouselTestimonial>
-				</div>
-				<ProfileCard
-					name='Fahan Ibrahim-Hashi'
-					position='Financial Advisor'
-					imageUrl={image}
-					bioPage='fahan'
-				></ProfileCard>
-			</div>
-		</main>
-	);
+    return (
+        <main className="min-h-screen h-fit">
+            {/* Section #1 */}
+            <div className="h-screen w-full bg-lilac flex flex-col items-center justify-around">
+                <div className="max-w-[950px] flex flex-col justify-between gap-6">
+                    <div className="flex gap-4">
+                        <div>
+                            <h1 className="font-playfair italic font-bold text-offWhite text-6xl mb-6">
+                                Wealth management to help you tie your camel
+                            </h1>
+                            <p className="font-source-sans text-offWhite text-2xl">
+                                Tailored personal finance by and for Muslim
+                                women
+                            </p>
+                        </div>
+                        <div className="flex-shrink-0 w-[200px] h-[200p]">
+                            <img
+                                className="w-full h-full object-cover"
+                                src="./camel_logo.png"
+                                alt="Picture of a cartoon camel"
+                            />
+                        </div>
+                    </div>
+                    <div className="flex gap-4">
+                        <Button variant={'outline'}>
+                            <Link to={'/about'}>Learn More</Link>
+                        </Button>
+                        <Button variant={'outline'}>
+                            <Link to={'/booking'}>Book My Session</Link>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+            {/* Section #2 */}
+            <div className="h-screen w-full bg-offWhite flex flex-col items-center justify-around">
+                {' '}
+                <CarouselQuote quotes={quotes} />
+                <div className="flex justify-center">
+                    <CarouselTestimonial
+                        testimonials={testimonials}
+                    ></CarouselTestimonial>
+                </div>
+            </div>
+            {/* Section #3 */}
+            <div className="h-screen w-full bg-offWhite flex flex-col items-center justify-around">
+                <ProfileCard
+                    name="Fahan Ibrahim-Hashi"
+                    position="Financial Advisor"
+                    imageUrl={fahanImage}
+                    bioPage="fahan"
+                ></ProfileCard>
+            </div>
+        </main>
+    );
 }
 
 export default Home;

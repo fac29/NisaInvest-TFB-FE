3e7b02930daba0fadf5b6bc88f6b886143d86b54
Alexander <alex@alexvoiceover.com>
2024-08-13 21:28:25 +0100

    Redeploy with log for supabaseURL

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+console.log("Supabase URL" + supabaseUrl);
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {

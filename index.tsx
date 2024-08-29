9545b2ee7426d431376148e042f1c3f4a9992294
diddydeeable <diddydeeable@gmail.com>
2024-08-29 14:28:01 +0100

    removed filed

--- index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App: React.FC = () => {
-  return <h1>Hello, TypeScript React!</h1>;
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
